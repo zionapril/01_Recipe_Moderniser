68934c854598f744b607f1e281069e926dccb729
zionapril <61804042+zionapril@users.noreply.github.com>
2020-03-10 12:03:27 +1300

    not_blank partial

---
+++ 01_not_blank_checker.py
@@ -0,0 +1,20 @@
+# Get's recipe name and checks it is not blank
+
+
+# Not Blank Function goes here
+def not_blank(question):
+    valid = False
+    while not valid:
+        response = input(question)
+
+        if response == "":
+            continue
+
+        else:
+            return response
+
+# Main Routine goes here
+
+recipe_name = not_blank("What is the recipe name? ")
+
+print("You are making {}".format(recipe_name))
